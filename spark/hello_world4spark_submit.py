a82ebbe12c64b9b6d656a708a9ec5f0ffe99f58b
Dell EMC CTO AI Team <35441993+dellctods@users.noreply.github.com>
2018-02-16 13:50:01 -0600

    Update hello_world4spark_submit.py

--- spark/hello_world4spark_submit.py
+++ spark/hello_world4spark_submit.py
@@ -7,5 +7,5 @@ import os
 
 sc = SparkContext(conf=SparkConf().setMaster("yarn"))
 dummy_RDD = sc.parallelize(np.array(range(6*4096)), 6)
-print "the dummy RDD has tottally {} elements in {} partitions".format(dummy_RDD.count(), dummy_RDD.getNumPartitions())
+print "the dummy RDD has totally {} elements in {} partitions".format(dummy_RDD.count(), dummy_RDD.getNumPartitions())
 print "spark context is created successfully!"
