ef10a1ac58396f2107f7220c1d17dc1e30cb4cf5
Dell EMC CTO AI Team <35441993+dellctods@users.noreply.github.com>
2018-02-16 13:49:21 -0600

    Add files via upload

---
+++ spark/hello_world4spark_submit.py
@@ -0,0 +1,11 @@
+from pyspark import SparkContext
+from pyspark import SparkConf
+import numpy as np
+import os
+
+#set spark context
+
+sc = SparkContext(conf=SparkConf().setMaster("yarn"))
+dummy_RDD = sc.parallelize(np.array(range(6*4096)), 6)
+print "the dummy RDD has tottally {} elements in {} partitions".format(dummy_RDD.count(), dummy_RDD.getNumPartitions())
+print "spark context is created successfully!"
